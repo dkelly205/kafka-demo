963ef1fb98563496e69933011092cd1f4fd56d89
dkelly205 <danieljkelly91@gmail.com>
2024-12-06 16:22:28 +0000

    saving changes

---
+++ src/main/java/com/dkelly205/kafka_demo/services/KafkaProducerService.java
@@ -0,0 +1,13 @@
+package com.dkelly205.kafka_demo.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class KafkaProducerService {
+
+    }
+
+}
--- src/main/java/com/dkelly205/kafka_demo/services/OrderService.java
+++ src/main/java/com/dkelly205/kafka_demo/services/OrderService.java
@@ -2,6 +2,7 @@ package com.dkelly205.kafka_demo.services;
 
 import com.dkelly205.kafka_demo.entity.CustomerEntity;
 import com.dkelly205.kafka_demo.entity.OrderEntity;
+import com.dkelly205.kafka_demo.enums.OrderStatus;
 import com.dkelly205.kafka_demo.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,10 +15,10 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
 
-
     public void saveOrder(BigDecimal totalAmount, CustomerEntity customer) {
         OrderEntity order = OrderEntity.builder().totalAmount(totalAmount)
                 .customer(customer)
+                .orderStatus(OrderStatus.PLACED)
                 .build();
         orderRepository.save(order);
     }
